5b2c9db765fd4ca36e7986d756b26e16c78e64c8
Hkaklot3015s <munjanijay421@gmail.com>
2025-04-22 10:18:26 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,8 @@ const connection = mysql.createConnection({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: 3306,
+    // connectTimeout: 10000,
 });
 
 connection.connect(function (err) {
