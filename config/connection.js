d25c1e0edfaeb99e24e1b40f51715249e32bffde
Hkaklot3015s <munjanijay421@gmail.com>
2025-04-22 10:27:35 +0530

    Change

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,21 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: 3306,
-    ssl: {
-        rejectUnauthorized: true
-    }
-    // connectTimeout: 10000,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE,
+    // port: 3306,
+    // ssl: {
+    //     rejectUnauthorized: true
+    // }
+    // // connectTimeout: 10000,
+    host: "mysql-4646e87-munjanijay421-29a8.k.aivencloud.com",
+    user: 'avnadmin',
+    password: "AVNS_PrwXNVTb-QYXBiCtWdJ",
+    database: 'defaultdb',
+    port: '12023',
+    connectTimeout: 10000,
 });
 
 connection.connect(function (err) {
